b02b92fccc473a73d3d3d3469d7aea70277adecf
om3rcitak <mail@omercitak.com>
2019-03-04 22:44:24 +0300

    add countries seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('country')->nullable();
+            $table->string('job')->nullable();
             $table->string('role');
             $table->string('language', 2);
             $table->rememberToken();
