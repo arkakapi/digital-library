8170198f2701feedbfd489a074f600bf86d99b72
om3rcitak <mail@omercitak.com>
2019-03-05 20:54:18 +0300

    create issues model and migration

---
+++ database/migrations/2019_03_05_173539_create_issues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug');
+            $table->string('title');
+            $table->float('price');
+            $table->string('cover');
+            $table->string('month');
+            $table->longText('content');
+            $table->string('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+}
