41cdf7313367ae9d908768387f55dcaa63aa67bc
om3rcitak <mail@omercitak.com>
2019-03-11 01:25:49 +0300

    localizate password reset email after added new user

--- app/Listeners/SendEmailWelcome.php
+++ app/Listeners/SendEmailWelcome.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\UserAdded;
 use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Password;
 
 class SendEmailWelcome
@@ -26,6 +27,7 @@ class SendEmailWelcome
      */
     public function handle(UserAdded $event)
     {
+        App::setLocale($event->user->language);
         Password::sendResetLink(['email' => $event->user->email], function (Message $message) {
             $message->subject($this->getEmailSubject());
         });
