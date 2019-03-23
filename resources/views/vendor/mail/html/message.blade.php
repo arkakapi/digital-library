8941674f0bce4eb44278893f8c8cf9d0d15c55f8
om3rcitak <mail@omercitak.com>
2019-03-23 22:13:43 +0300

    fix email title l10n problem

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
     {{-- Header --}}
     @slot('header')
         @component('mail::header', ['url' => config('app.url')])
-            {{ config('app.name') }}
+            {{ trans('app.name') }}
         @endcomponent
     @endslot
 
@@ -21,7 +21,7 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+            © {{ date('Y') }} {{ trans('app.name') }}. @lang('All rights reserved.')
         @endcomponent
     @endslot
 @endcomponent
